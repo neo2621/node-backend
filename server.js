adf0b47858ab916e719e22b174ae296d887d6f59
neo2621 <neomakhubo25@gmail.com>
2023-04-14 08:27:06 +0200

    Routes update

--- server.js
+++ server.js
@@ -41,32 +41,3 @@ db.mongoose
     console.log("Cannot connect to the database!", err);
     process.exit();
   });
-
-  const tutorials = require("./app/controllers/tutorial.controller.js")
-  
-  var router = require("express").Router();
-
-  // Create a new Tutorial
-  router.post("/", tutorials.create);
-
-  // Retrieve all Tutorials
-  router.get("/", tutorials.findAll);
-
-  // Retrieve all published Tutorials
-  router.get("/published", tutorials.findAllPublished);
-
-  // Retrieve a single Tutorial with id
-  router.get("/:id", tutorials.findOne);
-
-  // Update a Tutorial with id
-  router.put("/:id", tutorials.update);
-
-  // Delete a Tutorial with id
-router.delete("/:id", tutorials.delete);
-
-router.patch("/:id", tutorials.patch);
-
-// Delete all Tutorials
-router.delete("/", tutorials.deleteAll);
-
-app.use('/api/tutorials', router);
