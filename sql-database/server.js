c404b9fa321a2ee22e6a59ea30366100c9d37340
neo2621 <neomakhubo25@gmail.com>
2023-04-17 16:07:39 +0200

    updated2

--- sql-database/server.js
+++ sql-database/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const userroutes = require(`./src/users/routes`);
+const userRoutes = require(`./src/users/routes`);
 const app = express()
 const port = 3000
 
@@ -15,7 +15,7 @@ app.get('/', (request, response) => {
     response.json({ info: 'Node.js, Express, and Postgres API' })
   })
 
-  app.use(`./api/v1/users`, userroutes);
+  app.use(`./api/v1/users`, userRoutes);
 
   app.listen(port, () => {
     console.log(`App running on port ${port}.`)
